2e41e07dc8ffc7585e1030fea0038f542d2cf2a4
nfcarbone <nfcarbone@gmail.com>
2021-01-31 18:13:54 -0500

    passable style

--- src/JS808.js
+++ src/JS808.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
 
+function initCells() {
+  const cells = [];
+  for (let i = 0; i < 16; i++) {
+    cells[i] = 0;
+  }
+  return cells;
+}
+
 const INIT_EMPTY = () => {
   const empty = [];
   for (let i = 0; i < 16; i++) {
@@ -26,10 +34,6 @@ export default function JS808() {
     console.log(`playing: ${playing}`);
   }, [playing]);
 
-  //   useEffect(() => {
-  //     console.log(`clock: ${clock}`);
-  //   }, [clock]);
-
   // timeline clock
   const interval = useRef(null);
   useEffect(() => {
@@ -50,9 +54,14 @@ export default function JS808() {
     if (playing) {
       const cells = document.querySelectorAll(`.cell-${clock}`);
       cells.forEach((cell) => {
-        cell.classList.remove('on');
-        void cell.offsetWidth; // rm>offset>add to reset css animation
-        cell.classList.add('on');
+        if (
+          cell.classList.contains('full') ||
+          cell.classList.contains('half')
+        ) {
+          cell.classList.remove('on');
+          void cell.offsetWidth; // rm>offset>add to reset css animation
+          cell.classList.add('on');
+        }
       });
     }
   }, [playing, clock]);
@@ -88,31 +97,44 @@ export default function JS808() {
         </div>
         <select id='sequence-select'></select>
       </div>
-      <div id='sequencer'>
-        <div id='timeline'>This will be the timeline row</div>
-        <Instrument
-          inst={'kick'}
-          pattern={pattern.kick}
-          toggleCell={toggleCell}
-        />
-        <Instrument
-          inst={'snr'}
-          pattern={pattern.snr}
-          toggleCell={toggleCell}
-        />
-        <Instrument inst={'oh'} pattern={pattern.oh} toggleCell={toggleCell} />
-        <Instrument inst={'ch'} pattern={pattern.ch} toggleCell={toggleCell} />
+      <div id='sequencer-container'>
+        <div id='sequencer'>
+          <div id='timeline' className='inst'>
+            <div className='inst-label'></div>
+            <div id='timeline-grid' className='inst-grid'>
+              {initCells().map((cell, i) => {
+                return (
+                  <p key={`timeline-${i}`} className={`cell cell-${i} full tl`}>
+                    {i + 1}
+                  </p>
+                );
+              })}
+            </div>
+          </div>
+          <Instrument
+            inst={'kick'}
+            pattern={pattern.kick}
+            toggleCell={toggleCell}
+          />
+          <Instrument
+            inst={'snr'}
+            pattern={pattern.snr}
+            toggleCell={toggleCell}
+          />
+          <Instrument
+            inst={'oh'}
+            pattern={pattern.oh}
+            toggleCell={toggleCell}
+          />
+          <Instrument
+            inst={'ch'}
+            pattern={pattern.ch}
+            toggleCell={toggleCell}
+          />
+        </div>
       </div>
     </div>
   );
-}
-
-function initCells() {
-  const cells = [];
-  for (let i = 0; i < 16; i++) {
-    cells[i] = 0;
-  }
-  return cells;
 }
 
 function Instrument({ inst, toggleCell, pattern }) {
@@ -124,23 +146,25 @@ function Instrument({ inst, toggleCell, pattern }) {
       <div className='inst-label'>
         <h2 id={label}>{inst}</h2>
       </div>
-      {cells.map((cell, i) => {
-        const id = inst + i;
-        return (
-          <div
-            key={id}
-            id={id}
-            className={
-              pattern[i] === 0
-                ? styles + ` cell-${i}`
-                : pattern[i] === 1
-                ? styles + ` cell-${i} full`
-                : styles + ` cell-${i} half`
-            }
-            onClick={() => toggleCell(inst, i)}
-          ></div>
-        );
-      })}
+      <div className='inst-grid'>
+        {cells.map((cell, i) => {
+          const id = inst + i;
+          return (
+            <div
+              key={id}
+              id={id}
+              className={
+                pattern[i] === 0
+                  ? styles + ` cell-${i}`
+                  : pattern[i] === 1
+                  ? styles + ` cell-${i} full`
+                  : styles + ` cell-${i} half`
+              }
+              onClick={() => toggleCell(inst, i)}
+            ></div>
+          );
+        })}
+      </div>
     </div>
   );
 }
